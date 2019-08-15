3e2e7de9c07f710a2f818234e012e9596d9b2489
shehab20089 <xxxegygamerxxx@gmail.com>
2019-08-15 21:17:27 +0200

    adding audio player packege and using it in the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,10 +9,63 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text(nouns[154]),
+          title: Text('xylophone'),
         ),
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note1.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note2.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note3.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note4.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note5.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  final AudioCache player = AudioCache();
+                  player.play('note7.wav');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
